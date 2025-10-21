08b1765b4c90e4cdfa1e327226ab5ff27cb4e015
shelser <shelserg@gmail.com>
2025-10-21 17:14:23 +0300

    added new files

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
+import { ToDo } from "../models/todo-item";
 
 export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([])
+
+
+        const createNewToDo = (text: string) => {
+            const newToDo: ToDo = {
+                id: todos.length,
+                text: text,
+                isDone: false
+            };
+            setTodos([...todos, newToDo])
+        };
+
+        const updateToDo = (toDoItem: ToDo) => {
+            const newTodos = todos.map((todo) => {
+                if (todo.id === toDoItem.id ) {
+                    todo.isDone = !todo.isDone
+                }
+                return todo
+            })
+            setTodos(newTodos)
+        };
+
+        const deleteToDo = (toDoItem: ToDo) => {
+            const newTodos = todos.filter((todo) => todo.id !== toDoItem.id);
+            setTodos(newTodos);
+        };
+
     return (
         <>
             <Header />
-            <Form />
-            <ToDoList />
+            <Form createNewToDo={createNewToDo}/>
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
         </>
     )
 }
