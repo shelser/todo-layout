adff15c0e92587f318bb9771226052b3e97b4413
shelser <shelserg@gmail.com>
2025-10-22 18:09:46 +0300

    added new files

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,69 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
 import { ToDoListPage } from './pages/ToDoListPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ToDo } from './models/todo-item';
+import { NotFound } from './pages/404';
+import { ItemDescription } from './pages/ItemDescription';
+import { Layout } from './Layouts/Layout'
+
+
+const todos: ToDo[] = [
+  {
+    id: 0,
+    text: 'Первое действие',
+    isDone: false
+  },
+  {
+    id: 1,
+    text: 'Второе действие',
+    isDone: true
+  },
+  {
+    id: 2,
+    text: 'Третье действие',
+    isDone: false
+  },
+  {
+    id: 3,
+    text: 'Четвертое действие',
+    isDone: true
+  }
+]
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage todos={todos} />
+      },
+      {
+        path: '/todo',
+        element: <ToDoListPage />
+      },
+      {
+        path: '/list/:id',
+        element: <ItemDescription todos={todos}/>
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+], {basename: '/app/'})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Form } from "../components/Form/Form"
-import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
 import { ToDo } from "../models/todo-item";
 
@@ -34,7 +33,7 @@ export const ToDoListPage = () => {
 
     return (
         <>
-            <Header />
+            
             <Form createNewToDo={createNewToDo}/>
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
         </>
